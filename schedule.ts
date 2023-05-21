8cf4049e82f4f69a3bd80bc64839e4a4c0ef098d
Mente <dev@imkreative.com>
2023-05-20 22:41:48 -0400

    Revert schedule

--- schedule.ts
+++ schedule.ts
@@ -2,7 +2,7 @@ import { Cron } from "https://deno.land/x/croner@5.3.5/src/croner.js";
 
 if (import.meta.main) {
     console.log('Scheduler Started')
-    const _scheduler = new Cron("38 22 * * *", { timezone: 'America/New_York'}, async () => {
+    const _scheduler = new Cron("15 1 * * *", { timezone: 'America/New_York'}, async () => {
         console.log('Loading updated DB');
         await fetch(Deno.env.get('LOAD_URL')!, {
             method: 'POST',
