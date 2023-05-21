b291032df7880c4bbc1caf1ab46388df5f69fd82
Mente <clemente@activetheory.net>
2023-05-20 22:34:42 -0400

    Setup Scheduler

--- schedule.ts
+++ schedule.ts
@@ -1,11 +1,16 @@
 import { Cron } from "https://deno.land/x/croner@5.3.5/src/croner.js";
-import { load } from "./anidb.ts";
 
 if (import.meta.main) {
     console.log('Scheduler Started')
     const _scheduler = new Cron("15 1 * * *", { timezone: 'America/New_York'}, async () => {
         console.log('Loading updated DB');
-        await load({ db: Deno.env.get('ANIDB_DB')! });
+        await fetch(Deno.env.get('LOAD_URL')!, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${Deno.env.get('ANIDB_SECRET')}`
+            }
+        });
+
         console.log('DB updated');
     });
 }
